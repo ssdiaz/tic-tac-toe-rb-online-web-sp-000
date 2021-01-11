609515c51f49b59bca48d450bc8ae89dccf59c76
Sam D <samidiaz3@gmail.com>
2021-01-11 02:06:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,14 @@
 # win combinations
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5]  # Middle row
-]
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Column Left vertical
+  [1,4,7], # Column Middle vertical
+  [2,5,8], # Column Right vertical
+  [0,4,8], # Right to Left diagonal
+  [2,4,6]  # Left to Right diagonal
+   ]
 
 # prints current board representation
 def display_board(board)
