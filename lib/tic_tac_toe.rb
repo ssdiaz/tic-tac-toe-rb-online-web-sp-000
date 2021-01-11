5a8149305638515efb0d90755a8d6ba1af70d90f
Sam D <samidiaz3@gmail.com>
2021-01-11 02:06:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,10 +4,16 @@ WIN_COMBINATIONS = [
   [3,4,5]  # Middle row
 ]
 
+# prints current board representation
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# converts user input to represent on board's index
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
