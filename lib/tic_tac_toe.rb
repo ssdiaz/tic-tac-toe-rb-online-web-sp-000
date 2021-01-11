57800b9b8ec9952c72464416223da3f82a7eb060
Sam D <samidiaz3@gmail.com>
2021-01-11 03:56:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-# win combinations
+# possible win combinations
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
@@ -10,8 +10,7 @@ WIN_COMBINATIONS = [
   [2,4,6]  # Left to Right diagonal
 ]
 
-# prints current board representation
-def display_board(board)
+def display_board(board) # prints current board representation
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -19,23 +18,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# converts user input to represent on board's index
-def input_to_index(user_input)
+def input_to_index(user_input) # converts user input to represent on board's index
   user_input.to_i - 1
 end
 
-# check if position is free; returns true if position is taken, & false if position is free
-def position_taken?(board, index)
+def position_taken?(board, index) # returns true if position is taken, & false if position is free
   board[index] != " "
 end
 
-# true if move is valid: 1. input index between 0-8 2. space is not taken (is free)
-def valid_move?(board, index) #  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(board, index) # true if move is valid, as in: 1. input index between 0-8, 2. space is free/not taken
   index.between?(0, 8) && !position_taken?(board, index)
 end
 
-# retuns # of turns that have been played
-def turn_count (board)
+def turn_count (board) # returns number of turns that have been played
   turns = 0
   board.each do |token|
     if token == "X" || token == "O"
@@ -45,18 +40,15 @@ def turn_count (board)
   turns
 end
 
-# determines if X turn (evens) or O turn (odd)
-def current_player (board)
+def current_player (board) # determines if X turn (evens) or O turn (odd)
   turn_count(board).even? ? "X" : "O"
 end
 
-# sets index to current player's token
-def move(board, index, current_player)
+def move(board, index, current_player) # move sets index to current player's token
   board[index] = current_player
 end
 
-# represents a single turn move
-def turn(board)
+def turn(board) # represents a single turn move
   puts 'Please enter 1-9:'
   user_input = gets.strip
   index = input_to_index(user_input)
@@ -68,7 +60,6 @@ def turn(board)
   end
 end
 
-
 def won?(board) # return winning combo index, or false if no win combo
  WIN_COMBINATIONS.find do |winning_combo| # find first winning_combo in board (array of 3, ex: [0,1,2]) in WIN_COMBINATIONS
     #if (board[winning_combo[0]] == "X" && board[winning_combo[1]] == "X" && board[winning_combo[2]]  == "X") || (board[winning_combo[0]] == "O" && board[winning_combo[1]] == "O" && board[winning_combo[2]]  == "O")
@@ -79,16 +70,16 @@ def won?(board) # return winning combo index, or false if no win combo
   end
 end
 
-def full?(board) # accept a board and return true if every element in the board contains either x or o
+def full?(board) # accept a board and return true if every element in the board contains either X or O
   board.all? {|index| index == "X" || index == "O"}
 end
 
-def draw?(board)   # returns true if board not won but full
+def draw?(board)   # returns true if board not won but full for a draw
   !won?(board) && full?(board)
   end
 
-def over?(board) # true if won, draw, or full
-  won?(board) || draw?(board) || full?(board)
+def over?(board) # true if won, draw, or full for game to be over (don't need full as this is actioned in #draw?)
+  won?(board) || draw?(board) # || full?(board)
 end
 
 def winner(board) # returns the token that won
