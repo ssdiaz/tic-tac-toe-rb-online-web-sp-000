a7f6a17699fc4aaea2be335d9d3a016cd5e0301c
Sam D <samidiaz3@gmail.com>
2021-01-11 03:11:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,9 +73,9 @@ def won?(board) # return winning combo index, or false if no win combo
  WIN_COMBINATIONS.find do |winning_combo| # find first  winning_combo in WIN_COMBINATIONS
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
     #if (board[winning_combo[0]] == "X" && board[winning_combo[1]] == "X" && board[winning_combo[2]]  == "X") || (board[winning_combo[0]] == "O" && board[winning_combo[1]] == "O" && board[winning_combo[2]]  == "O")
-    if board[winning_combo[0]] == board[winning_combo[1]] &&
-        board[winning_combo[0]] == board[winning_combo[2]] &&
-          board[winning_combo[1]] == board[winning_combo[2]] &&
+    if board[winning_combo[0]] == board[winning_combo[1]] == board[winning_combo[2]]  &&
+      #  board[winning_combo[0]] == board[winning_combo[2]] &&
+        #  board[winning_combo[1]] == board[winning_combo[2]] &&
             board[winning_combo[0]] != " "
         return winning_combo # return the win_combination indexes that won
       end
