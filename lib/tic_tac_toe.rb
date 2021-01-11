fc0898444c4ce01d73bc547af72ee68308cd8722
Sam D <samidiaz3@gmail.com>
2021-01-11 02:22:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,3 +60,14 @@ def current_player (board)
 end
 
 # represents a single turn move
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
