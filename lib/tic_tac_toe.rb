1f387b45acc2546ae34cea06d5ac2f970703444a
Sam D <samidiaz3@gmail.com>
2021-01-11 03:24:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,3 +96,16 @@ def winner(board) # returns the token that won
    board[won?(board)[0]] # find first index of winning board and display it from board
   end
 end
+
+# play method, responsi for game loop,
+def play(board)
+#  until the game is over
+    turn(board) #take turns
+#  end
+#
+#  if the game was won
+#    congratulate the winner
+#  else if the game was a draw
+#    tell the players it has been a draw
+#  end
+end
