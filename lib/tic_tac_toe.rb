fb089fd9104014dd975516b8f21f01d102cd94c7
Sam D <samidiaz3@gmail.com>
2021-01-11 01:15:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,3 +82,23 @@ def turn(board)
     turn(board)                            #  ask for input again until given valid input (loop back to top)
   end
 end
+
+# returns false if no win combo is present on the board & reutrn winning index array [0,1,2]
+def won?(board)
+ WIN_COMBINATIONS.find do |win_combination| #  find first win combo in WIN_COMBINATIONS
+    # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+    win_index_1 = win_combination[0] # grab each index from the win_combination that composes a win. [left]
+    win_index_2 = win_combination[1] # grab each index from the win_combination that composes a win. [middle]
+    win_index_3 = win_combination[2] # grab each index from the win_combination that composes a win. [right]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination # return the win_combination indexes that won.
+      else
+        false
+      end
+  end
+end
